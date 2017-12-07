fca4275b1e1b087ea2f11f4ea90c502027a89674
Varma <Varma@DESKTOP-1TVJB5S>
2017-12-07 15:28:16 +0530

    Token +6598+ added towards git commit

--- new-project-git-commiting-test/src/main/java/my/first/TestClass.java
+++ new-project-git-commiting-test/src/main/java/my/first/TestClass.java
@@ -4,5 +4,6 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		System.out.println("This is a testing commit to test git.......");
+		System.out.println("Line number 983275 added");
 	}
 }
