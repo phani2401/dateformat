ea49d63d36171ec34a395acd2506dc5c72c9a65c
Varma <Varma@DESKTOP-1TVJB5S>
2017-12-07 15:26:57 +0530

    Token 001245 Commiting towards git

--- new-project-git-commiting-test/src/main/java/my/first/TestClass.java
+++ new-project-git-commiting-test/src/main/java/my/first/TestClass.java
@@ -3,6 +3,6 @@ package my.first;
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("This is a testing commit to test git");
+		System.out.println("This is a testing commit to test git.......");
 	}
 }
